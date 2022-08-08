63f60b9c99da7dd5753142261260dc865afe91ba
Noldor99 <qwerr19942017@gmail.com>
2022-08-08 16:10:34 +0300

    Next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [value, setValue]=useState('')
+  const [number, setNumber]=useState(0)
+  const [changes, setChanges]=useState('default')
+
+  var Array =["button","console","change","align"]
+
+  function increment(){
+    setNumber(number+1)
+    console.log(Array[number]) 
+  }
+  
+  function dicrement(){
+    setNumber(number-1)
+    console.log(Array[number]) 
+  }
+
+  useEffect(()=>{
+    if(value===Array[number]){
+      setChanges("True")
+    }
+    if(value!=Array[number]){
+      setChanges("default")
+    }
+
+  },[value])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: "1000px", margin:"0 auto"}} className="App">
+
+      <h1>{number}</h1>
+      <h1>{changes}</h1>
+      <input 
+      style={{fontSize:"50px"}}
+      type="text"
+      onChange={event=>setValue(event.target.value)}
+      />
+      <button onClick={increment}>increment</button>
+      <button onClick={dicrement}>dicrement</button>
+      <h3 style={{textAlign: "end",paddingTop:"400px"}}>{Array[number]}</h3>
+
     </div>
   );
 }
